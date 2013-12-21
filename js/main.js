1ab05424e830af1f5af4a1a54bc3f82e5935d71b
Sean Graham <github@sean-graham.com>
2013-12-20 22:05:55 -0500

    switched to highcharts because I'm not interested in reinventing the wheel.

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ var ds = new Miso.Dataset({
 ds.fetch({
     success : function() {
         var data = [];
+        var categories = [];
 
         // Experimented with collapsing small values into an "Other"
         // category.
@@ -17,7 +18,8 @@ ds.fetch({
 
         this.countBy("Location").each(function(row){ 
             if(row.count > otherThreshold) {
-                data.push({name: row.Location, value: +row.count}); 
+                data.push(+row.count); 
+                categories.push(row.Location);
             }
             else {
                 otherCount += row.count;
@@ -25,51 +27,35 @@ ds.fetch({
         });
 
         if(otherCount > 0) {
-            data.push({name: "Other", value: otherCount}); 
+            data.push(otherCount); 
+            categories.push("Other");
         }
 
-        createBarGraph(data, 400, 20); 
+        $(function () { 
+            $('#container').highcharts({
+                chart: {
+                    type: 'bar'
+                },
+                title: {
+                    text: 'Theatre Frequency'
+                },
+                xAxis: {
+                    categories: categories
+                },
+                yAxis: {
+                    title: {
+                        text: 'Number of Visits'
+                    }
+                },
+                series: [{
+                    data: data,
+                    name: "Visits",
+                    showInLegend: false
+                }]
+            });
+        });
     },
     error : function() {
         // Data loading failed
     }
 });
-
-var createBarGraph = function(data, width, barHeight) {
-    var x = d3.scale.linear().range([0, width - 160]);
-    x.domain([0, d3.max(data, function(d) { 
-        return d.value; 
-    })]);
-
-    var chart = d3.select(".chart")
-        .attr("width", width)
-        .attr("height", barHeight * data.length);
-
-    var bar = chart.selectAll("g")
-        .data(data)
-        .enter().append("g")
-        .attr("transform", function(d, i) { 
-            return "translate(0," + i * barHeight + ")"; 
-        });
-
-    bar.append('text')
-        .text(function(d){ return d.name; })
-        .attr('text-anchor', 'end')
-        .attr('height', 25)
-        .attr('class', 'nameLabel')
-        .attr("dy", ".35em")
-        .attr('x', 140)
-        .attr("y", barHeight / 2);
-
-    bar.append("rect")
-        .attr("width", function(d) { return x(d.value); })
-        .attr("height", barHeight - 1)
-        .attr("x", 150);
-
-    bar.append("text")
-        .attr("x", function(d) { return x(d.value) + 150 - 2; })
-        .attr("y", barHeight / 2)
-        .attr('class', 'valueLabel')
-        .attr("dy", ".35em")
-        .text(function(d) { return d.value; });
-};
